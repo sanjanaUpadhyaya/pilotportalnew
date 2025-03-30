d355de4560092b8b0324ba389c2f7f25e5802899
Sanjana S <148531386+sanjanaUpadhyaya@users.noreply.github.com>
2025-03-30 12:18:33 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,48 +1,8 @@
-// require("dotenv").config();
-// // const { Pool } = require("pg");
-// // const isProduction=process.env.NODE_ENV==="production";
-// // const connectionstring="postgres://yglpwsih:CWRcHriU-lfjazbDAfnD0hvf3f2UZbWz@isilo.db.elephantsql.com/yglpwsih"
-// // const pool=new Pool({
-// //     connectionString: isProduction?process.env.DATABASE_URL:connectionstring
-// // });
-// // module.exports={pool};
-
-// var pg = require('pg');
-// //or native libpq bindings
-// //var pg = require('pg').native
-
-// var conString = "postgres://yglpwsih:CWRcHriU-lfjazbDAfnD0hvf3f2UZbWz@isilo.db.elephantsql.com/yglpwsih" //Can be found in the Details page
-// var client = new pg.Client(conString);
-// client.connect(function(err) {
-//   if(err) {
-//     return console.error('could not connect to postgres', err);
-//   }
-//   client.query('SELECT NOW() AS "theTime"', function(err, result) {
-//     if(err) {
-//       return console.error('error running query', err);
-//     }
-//     console.log(result.rows[0].theTime);
-//     // >> output: 2018-08-23T14:02:57.117Z
-//     client.end();
-//   });
-// });
-// module.exports=pg
-
-
-
-
-
-// const { Pool } = require("pg");
-// const config = "postgres://qfxwijux:sGtZVyXqp9PIG1EaLpZNdZX9qO1RceyE@mouse.db.elephantsql.com/qfxwijux";
-// const pool = new Pool({ connectionString: config, ssl: { rejectUnauthorized: false } });
-// module.exports = pool;
-
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const config = {
-  //connectionString: "postgresql://neondb_owner:npg_Ir3pyohAHJO8@ep-flat-pond-a1qfpn19-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
-  //witout ssl in connectionstring
-  connectionString: "postgresql://neondb_owner:npg_Ir3pyohAHJO8@ep-flat-pond-a1qfpn19-pooler.ap-southeast-1.aws.neon.tech/neondb",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
